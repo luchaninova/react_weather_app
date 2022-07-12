062f225c1a5f9b2f2834798add1275f7353632c2
luchaninova <106754378+luchaninova@users.noreply.github.com>
2022-07-12 15:06:29 +0300

    Add the footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <footer>
+        <p>
+          <a
+            href="https://github.com/luchaninova/react_weather_upp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-soursed progect
+          </a>{" "}
+          by Irina Luchaninova
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
