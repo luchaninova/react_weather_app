1f956b3781270622e6a8f006a5523ec6b26e2b74
luchaninova <106754378+luchaninova@users.noreply.github.com>
2022-07-13 15:21:26 +0300

    Updated bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <footer>
-        <p>
-          <a
-            href="https://github.com/luchaninova/react_weather_upp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-soursed progect
-          </a>{" "}
-          by Irina Luchaninova
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <button className="btn btn-primary">Click</button>
+        <footer>
+          <p>
+            <a
+              href="https://github.com/luchaninova/react_weather_upp"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-soursed progect
+            </a>{" "}
+            by Irina Luchaninova
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
