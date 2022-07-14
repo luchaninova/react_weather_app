c0ee78349f640ec7bd6052a8ae7a1a2f82d666e6
luchaninova <106754378+luchaninova@users.noreply.github.com>
2022-07-14 16:03:00 +0300

    Added API weather integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="CitySearch">
-        <div className="row">
-          <div className="col-9">
-            <form className="search">
-              <input
-                type="text"
-                placeholder="Enter a city here"
-                autoFocus="on"
-              />
-            </form>
+export default function Weather(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState([]);
+
+  function getCurrentWeather(response) {
+    setWeather({
+      description: response.data.weather[0].main,
+      icon: response.data.weather[0].icon,
+      humidity: Math.round(response.data.main.humidity),
+      temperature: Math.round(response.data.main.temp),
+      //time: new DateUtil(new Date(response.data.dt * 1000)).dayTime(),
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+    });
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        <div className="CitySearch">
+          <div className="row">
+            <div className="col-9">
+              <form className="search">
+                <input
+                  type="text"
+                  placeholder="Enter a city here"
+                  autoFocus="on"
+                />
+              </form>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-primary w-100">Search</button>
+            </div>
           </div>
-          <div className="col-3">
-            <button className="btn btn-primary w-100">Search</button>
+        </div>
+        <div className="CurrentWeather">
+          <h1>{weather.city}</h1>
+          <ul>
+            <li>Monday, 15:20</li>
+            <li>{weather.description}</li>
+          </ul>
+          <div className="row">
+            <div className="col-8">
+              <img src={weather.icon} alt={weather.description} />
+              <span className="temperature">{weather.temperature}</span>
+              <span className="unit">℃</span>
+            </div>
+            <div className="col-4">
+              <ul>
+                <li>Wind</li>
+                <p className="wind">{weather.wind} m/h</p>
+                <li>Humidity</li>
+                <p className="humidity">{weather.humidity}%</p>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-      <div className="CurrentWeather">
-        <h1>London</h1>
-        <ul>
-          <li>Monday, 15:20</li>
-          <li>Scattered Clouds</li>
-        </ul>
-        <div className="row">
-          <div className="col-8">
-            <span className="temperature">6</span>
-            <span className="unit">℃</span>
-          </div>
-          <div className="col-4">
-            <ul>
-              <li>Wind</li>
-              <p className="wind">5.66 m/h</p>
-              <li>Humidity</li>
-              <p className="humidity">47%</p>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "b26c02eb24c77058192e800dec3f2e04";
+    let apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiLink).then(getCurrentWeather);
+    return "Loading...";
+  }
 }
